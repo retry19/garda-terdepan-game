7aa7292ee6164ce6582191245befadeb76a3280e
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-05-28 03:18:54 +0700

    fungsi balik badan
    menambahkan fungsi balik badan ketika karakter bergerak ke kanan atau kiri

--- Assets/Scripts/Players/Movement.cs
+++ Assets/Scripts/Players/Movement.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private int kecepatan = 7;
+    private int kecepatan = 7,
+        kekuatanLompat = 50,
+        pindah;
+    private bool balik;
+
+    Rigidbody2D lompat;
+
+    void Start()
+    {
+        lompat = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector2.right * kecepatan * Time.deltaTime);
+            pindah = -1;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-Vector2.right * kecepatan * Time.deltaTime);
+            pindah = 1;
         }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            lompat.AddForce(new Vector2(0, kekuatanLompat));
+        }
+
+        if (pindah > 0 && !balik)
+        {
+            BalikBadan();
+        } else if (pindah < 0 && balik)
+        {
+            BalikBadan();
+        }
+    }
+
+    void BalikBadan()
+    {
+        balik = !balik;
+        Vector3 karakter = transform.localScale;
+        karakter.x *= -1;
+        transform.localScale = karakter;
     }
 }

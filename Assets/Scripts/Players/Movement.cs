ada112b1a5e3322053e3faa73fb625e0d4083ad9
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-05-27 18:24:02 +0700

    menambah script movement player
    gerak ke kanan dan kiri

---
+++ Assets/Scripts/Players/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private int kecepatan = 7;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * kecepatan * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * kecepatan * Time.deltaTime);
+        }
+    }
+}

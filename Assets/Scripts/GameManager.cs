127cad19c046d597320e410fde61632475a58a73
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-06-15 19:38:43 +0700

    add canvas end game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject panelWinGame,
+        panelLoseGame,
+        canvasUI;
+
+    private Items itemsPlayer;
+
+    void Start()
+    {
+        itemsPlayer = GameObject.FindWithTag("Character").GetComponent<Items>();
+
+        panelWinGame = GameObject.Find("PanelWinGame");
+        panelLoseGame = GameObject.Find("PanelLoseGame");
+        canvasUI = GameObject.Find("Canvas");
+
+        SetActiveRecursivelyExt(panelWinGame, false);
+        SetActiveRecursivelyExt(panelLoseGame, false);
+    }
+
+    void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if(enemies.Length == 0)
+            EndGame(true);
+
+        if (itemsPlayer.health <= 0)
+            EndGame(false);
+    }
+
+    private void EndGame(bool isWin)
+    {
+        if(isWin == true)
+        {
+            SetActiveRecursivelyExt(panelWinGame, true);
+        } else
+        {
+            SetActiveRecursivelyExt(panelLoseGame, true);
+        }
+        
+        SetActiveRecursivelyExt(canvasUI, false);
+        Debug.Log("Game has End!");
+    }
+
+    private void SetActiveRecursivelyExt(GameObject obj, bool state)
+    {
+        obj.SetActive(state);
+        foreach (Transform child in obj.transform)
+        {
+            SetActiveRecursivelyExt(child.gameObject, state);
+        }
+    }
+}

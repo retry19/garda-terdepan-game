b07a4a1b7048edad0948cb1bc06e42b9a70ed2e4
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-06-18 21:00:25 +0700

    add gui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,12 @@
 
 public class GameManager : MonoBehaviour
 {
+    public static bool isPaused = false;
+
     private GameObject panelWinGame,
         panelLoseGame,
-        canvasUI;
+        canvasUI,
+        panelPause;
 
     private Items itemsPlayer;
 
@@ -15,9 +18,11 @@ public class GameManager : MonoBehaviour
         panelWinGame = GameObject.Find("PanelWinGame");
         panelLoseGame = GameObject.Find("PanelLoseGame");
         canvasUI = GameObject.Find("Canvas");
+        panelPause = GameObject.Find("PanelPause");
 
         SetActiveRecursivelyExt(panelWinGame, false);
         SetActiveRecursivelyExt(panelLoseGame, false);
+        SetActiveRecursivelyExt(panelPause, false);
     }
 
     void Update()
@@ -27,17 +32,52 @@ public class GameManager : MonoBehaviour
         if(enemies.Length == 0)
             EndGame(true);
 
-        if (itemsPlayer.health <= 0)
+        if(itemsPlayer.health <= 0)
             EndGame(false);
+
+        if(itemsPlayer.ammo == 0 && itemsPlayer.money < 10)
+            EndGame(false);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                GameResume();
+            }
+            else
+            {
+                GamePause();
+            }
+        }
+    }
+
+    public void GameResume()
+    {
+        SetActiveRecursivelyExt(panelPause, false);
+        SetActiveRecursivelyExt(canvasUI, true);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void GamePause()
+    {
+        SetActiveRecursivelyExt(panelPause, true);
+        SetActiveRecursivelyExt(canvasUI, false);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     private void EndGame(bool isWin)
     {
         if(isWin == true)
         {
+            FindObjectOfType<AudioManager>().Stop("EnemyAttack");
+            //FindObjectOfType<AudioManager>().Play("WinGame");
             SetActiveRecursivelyExt(panelWinGame, true);
         } else
         {
+            FindObjectOfType<AudioManager>().Stop("EnemyAttack");
+            //FindObjectOfType<AudioManager>().Play("LoseGame");
             SetActiveRecursivelyExt(panelLoseGame, true);
         }
         
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -7,5 +7,17 @@ public class SceneManager : MonoBehaviour
     public void ChangeScene(int scene)
     {
         Application.LoadLevel(scene);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMenu()
+    {
+        Application.LoadLevel(0);
+        Time.timeScale = 1f;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
